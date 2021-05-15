4da745cf0dbcd3f564c0861aec01f09ba65e3dc8
Daniel Kleebinder <daniel.kleebinder@gmail.com>
2021-05-15 22:46:43 +0200

    Use protected object entries as track barriers

--- railway/src/railway-tracks.adb
+++ railway/src/railway-tracks.adb
@@ -2,9 +2,28 @@ package body Railway.Tracks is
 
    
    protected body Track_Section is
-      procedure Enter is begin Occupied := True; end Enter;
-      procedure Leave is begin Occupied := False; end Leave;
-      function Is_Occupied return Boolean is begin return Occupied; end Is_Occupied;
+      
+      -- A train can only enter this train section if it is NOT occupied by any
+      -- other Train at the time. Trains will wait until the section if free again.
+      entry Enter
+        when not Occupied is
+      begin
+         Occupied := True;
+      end Enter;
+      
+      -- Trains can of course only leave a track section if it was occupied beforehand.
+      entry Leave
+        when Occupied is
+      begin
+         Occupied := False;
+      end Leave;
+      
+      -- Returns if this track section is currently occupied or not
+      function Is_Occupied return Boolean is
+      begin
+         return Occupied;
+      end Is_Occupied;
+      
    end Track_Section;
    
 
--- railway/src/railway-tracks.ads
+++ railway/src/railway-tracks.ads
@@ -1,11 +1,13 @@
 package Railway.Tracks is
    
    
-   -- Every track section of the railway system is a protected object which trains can occupy and release
-   -- at any time. Track sections also contain a pointer to the previous and the next track section.
+   -- Every track section of the railway system is a protected object which trains
+   -- can occupy and release at any time. Be careful: Entering and leaving a track
+   -- section might lead to deadlocks since track can only be entered if they are
+   -- free at the time.
    protected type Track_Section is
-      procedure Enter;
-      procedure Leave;
+      entry Enter;
+      entry Leave;
       function Is_Occupied return Boolean;
    private
       Occupied : Boolean := False;
